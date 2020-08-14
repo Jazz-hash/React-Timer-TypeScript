355c7f0e743dd0f87f4c9def26800033604a962c
Jazzel Mehmood <jazzelmehmood6@gmail.com>
2020-08-15 00:59:16 +0500

    Added alarm feature

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { TimerButton } from "../TimerButton/TimerButton";
+const alarm = require("./horse.mp3");
+const audio = new Audio(alarm);
 
 type TimerState = {
   minutes: number;
@@ -12,8 +14,8 @@ class Timer extends Component<{}, TimerState> {
   constructor(props: TimerState) {
     super(props);
     this.state = {
-      minutes: 25,
-      seconds: 0,
+      minutes: 0,
+      seconds: 5,
       isOn: false,
     };
 
@@ -55,9 +57,22 @@ class Timer extends Component<{}, TimerState> {
   resetTimer() {
     this.stopTimer();
     this.setState({
-      minutes: 25,
-      seconds: 0,
+      minutes: 0,
+      seconds: 5,
     });
+    audio.pause();
+  }
+
+  playAlarm() {
+    audio.load();
+    const audioPromise = audio.play();
+    if (audioPromise !== undefined) {
+      audioPromise
+        .then((_) => {})
+        .catch((err) => {
+          console.info(err);
+        });
+    }
   }
 
   render = () => {
@@ -65,26 +80,42 @@ class Timer extends Component<{}, TimerState> {
 
     return (
       <div className="timer-container">
-        <div className="time-display">
-          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-        </div>
-        <div className="timer-button-container">
-          <TimerButton
-            className="timer-button start-timer"
-            buttonAction={this.startTimer}
-            buttonValue={"Start"}
-          />
-          <TimerButton
-            className="timer-button stop-timer"
-            buttonAction={this.stopTimer}
-            buttonValue={"Stop"}
-          />
-          <TimerButton
-            className="timer-button reset-timer"
-            buttonAction={this.resetTimer}
-            buttonValue={"Reset"}
-          />
-        </div>
+        {minutes === 0 && seconds === 0 ? (
+          <>
+            <div className="time-display">bye byee</div>
+            {this.playAlarm()}
+            <div className="timer-button-container">
+              <TimerButton
+                className="timer-button reset-timer"
+                buttonAction={this.resetTimer}
+                buttonValue={"Reset"}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="time-display">
+              {`${minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`}
+            </div>
+            <div className="timer-button-container">
+              <TimerButton
+                className="timer-button start-timer"
+                buttonAction={this.startTimer}
+                buttonValue={"Start"}
+              />
+              <TimerButton
+                className="timer-button stop-timer"
+                buttonAction={this.stopTimer}
+                buttonValue={"Stop"}
+              />
+              <TimerButton
+                className="timer-button reset-timer"
+                buttonAction={this.resetTimer}
+                buttonValue={"Reset"}
+              />
+            </div>
+          </>
+        )}
       </div>
     );
   };
